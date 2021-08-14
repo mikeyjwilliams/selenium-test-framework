073679148512efd529dfc75834e15bd9e3c1c882
Michael Williams <williamsmikeyj@outlook.com>
2021-08-14 15:51:57 -0400

    fixing dir dancing

--- utility/utility.py
+++ utility/utility.py
@@ -10,13 +10,12 @@ import os
 import sys
 import inspect
 # fetch path to the directory in which current file is, from root directory or C:\ (or whatever driver number it is)
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-# extract the path to parent directory
-parentdir = os.path.dirname(currentdir)
-# insert path to the folder from parent directory from which the python module/ file is to be imported
-sys.path.insert(0, parentdir)
+
 
 # Base Methods to use in framework.
+
+
+
 
 class Utility():
 
