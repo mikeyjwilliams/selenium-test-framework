fcb123533c4b285d1de9630534d72c40186b6730
Michael Williams <williamsmikeyj@outlook.com>
2021-08-14 12:29:37 -0400

    comments added

---
+++ resources/Locators.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from selenium.webdriver.common.by import By
+'''
+usage of By import
+ID = "id"
+XPATH = "xpath"
+LINK_TEXT = "link text"
+PARTIAL_LINK_TEXT = "partial link text"
+NAME = "name"
+TAG_NAME = "tag name"
+CLASS_NAME = "class name"
+CSS_SELECTOR = "css selector"
+
+example: By.CLASS_NAME, 'class-name'
+
+Description:
+File for all properly named locators for paths using proper linkage from above.
+ex.
+constant_name = `tuple`(By.`names_from_above`, quoted=needed name)
+'''
