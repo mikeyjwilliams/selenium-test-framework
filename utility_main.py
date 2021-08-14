0b178d6cc3fe2739e0310d189632fc7e9b689868
Michael Williams <williamsmikeyj@outlook.com>
2021-08-14 17:06:39 -0400

    update

--- utility_main.py
+++ utility_main.py
@@ -1,8 +1,24 @@
-from utility.utility import Driver
+#!/usr/bin/env python3
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import os
+import sys
+import inspect
 
-chrome = Driver()
+#! usable to grab other created imports easier
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# extract the path to parent directory
+parentdir = os.path.dirname(currentdir)
+# insert path to the folder from parent directory from which the python module/ file is to be imported
+sys.path.insert(0, parentdir)
+# insert path to folder from parent directory.
+sys.path.insert(0, parentdir+'\\resources')
+sys.path.insert(0, parentdir+'\\utility')
 
-driver = chrome.main_webdriver('chrome')
+# from resources import Locators
+# from utility import utility 
 
-print(driver)
+
+
+
 
